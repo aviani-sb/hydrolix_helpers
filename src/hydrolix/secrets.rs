b7225fbfed586477651313a799dce4f8719b465a
James Aviani <james@hydrolix.io>
2025-01-13 22:01:01 -0800

    Added something to secrets

--- src/hydrolix/secrets.rs
+++ src/hydrolix/secrets.rs
@@ -1,13 +1,19 @@
 use serde::Deserialize;
 
-#[derive(Debug, Deserialize, Clone)]
+#[derive(Debug, Deserialize)]
+pub struct Config {
+    pub machines: Vec<Machine>,
+    pub git_snapshots: GitSnapshots,
+}
+
+#[derive(Debug, Deserialize)]
 pub struct Machine {
     pub base_url: String,
     pub username: String,
     pub password: String,
 }
 
-#[derive(Debug, Deserialize, Clone)]
-pub struct Config {
-    pub machines: Vec<Machine>,
+#[derive(Debug, Deserialize)]
+pub struct GitSnapshots {
+    pub token: String,
 }
